d162299958d5c8b54ca8f11589f15cc34ab5e75a
Srikrushna <palsrikrushna6@gmail.com>
2024-02-27 21:59:52 +0530

    Installed basic dependencies Intialize the npm in project: npm init Install parcel in the development environment: npm i -D parcel Installed react: 2024-02-27 20:52:31.473 [info] Log level: Info

--- Namaste_React_Practice/App.js
+++ Namaste_React_Practice/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading1 = React.createElement("h1", { id: "title1" }, "Namaste React Title 1!");
 const heading2 = React.createElement("h2", { id: "title2" }, "Namaste React Title 2!");
