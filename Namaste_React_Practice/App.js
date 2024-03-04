37ce47ccde3ff14ddd4b0e1567878a37da2b5ba3
Srikrushna <palsrikrushna6@gmail.com>
2024-03-04 15:43:35 +0530

    Video 4: Diffren way of writing functional component change in package.json Basic understanding of JSX

--- Namaste_React_Practice/App.js
+++ Namaste_React_Practice/App.js
@@ -1,9 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "title1" }, "Namaste React Title 1!");
-const heading2 = React.createElement("h2", { id: "title2" }, "Namaste React Title 2!");
-const container = React.createElement("div", { id: "container" }, [heading1, heading2]);
+const Title = () => {
+    return <h1 id="title" key="h1">Namaste React!</h1>
+}
+
+const Title2 = () => {
+    return (
+        <h2>Namaste React Title2!</h2>
+    )
+}
+
+
+const Title3 = () => {
+        return <h3>Namaste React Title3!</h3>
+}
+
+const Title4 = () => (
+    <h4>Namaste React Title4!</h4>
+)
+
+const title5 = () => (
+    <h5>
+        Namaste React Title5!
+    </h5>
+)
+
+const title6 = (
+    <h6>
+        Namaste React Title6!
+    </h6>
+)
+
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <Title />
+            <Title2 />
+            <Title3 />
+            <Title4 />
+            {title5()}
+            {title6}
+            <h2>Namaste React Functional Component</h2>
+            <h2>Heading tag 2</h2>
+        </div>
+    )
+}
+
+
+console.log("test babelrc");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent />);
